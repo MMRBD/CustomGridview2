19d401a53dd4725fce4c08ef2a600f2a8a0c95a9
mokle <moklesur508@gmail.com>
2017-04-25 12:11:23 +0600

    New

--- app/src/main/java/com/example/mokle/customgridview/GridAdapter.java
+++ app/src/main/java/com/example/mokle/customgridview/GridAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mokle.customgridview;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
 import java.util.zip.Inflater;
 
 /**
@@ -20,6 +22,7 @@ public class GridAdapter extends BaseAdapter {
     private String[] letterList;
     private int[] images;
     private LayoutInflater inflater;
+
 
     public GridAdapter(Context context, String[] letterList, int[] images) {
         this.context = context;
@@ -46,15 +49,23 @@ public class GridAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         View gridView = convertView;
+        Random rand = new Random();
+        int r = rand.nextInt(255);
+        int g = rand.nextInt(255);
+        int b = rand.nextInt(255);
+
+        int randomColor = Color.rgb(r,g,b);
 
         if (convertView == null){
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             gridView = inflater.inflate(R.layout.custom_layout, null);
+
+            gridView.setBackgroundColor(randomColor);
+
         }
 
         ImageView imageView = (ImageView) gridView.findViewById(R.id.letterImageView);
         TextView textView = (TextView) gridView.findViewById(R.id.letterTextView);
-
         imageView.setImageResource(images[position]);
         textView.setText(letterList[position]);
 
